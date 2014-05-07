9a567166f616eb3ccb296fc809aaf005c0631c9e
temoffey <temoffey@inbox.ru>
2014-05-08 05:35:30 +0600

    Добавление кнопок управления, добавление подсветки выделенного текста, добавление тем оформления

---
+++ js/client.js
@@ -0,0 +1,131 @@
+var Caret = document.getElementById("Caret")
+var codeEdit = document.getElementById("codeEdit")
+var codeView = document.getElementById("codeView")
+var editCaret = document.getElementById("editCaret")
+var str = codeEdit.value.length
+var carets = []
+var sessionid = ""
+
+document.getElementById("black_link").onclick = function() {
+	document.getElementById("black_style").disabled = true
+	document.getElementById("white_style").disabled = false
+	document.body.className = "white"
+	return false
+}
+
+document.getElementById("white_link").onclick = function() {
+	document.getElementById("white_style").disabled = true
+	document.getElementById("black_style").disabled = false
+	document.body.className = "black"
+	return false
+}
+
+var renderStr = function() {
+	codeView.innerHTML = codeEdit.value.replace(/</g, "&#60;").replace(/>/g, "&#62;")
+	codeView.className = "hljs"
+	hljs.highlightBlock(codeView)
+	codeEdit.style.height = codeView.offsetHeight + "px"
+}
+
+var renderCaret = function(id) {
+	document.getElementById("editCaret-"+id).innerHTML = codeEdit.value.substring(0, carets[id].begin).replace(/</g, "&#60;").replace(/>/g, "&#62;")
+	document.getElementById("Caret-"+id).innerHTML = codeEdit.value.substring(carets[id].begin, carets[id].end).replace(/</g, "&#60;").replace(/>/g, "&#62;")
+}
+
+var renderCarets = function() {
+	for (var s in carets) {
+		renderCaret(s)
+	}
+}
+
+var handler = function() {
+
+	if (str != codeEdit.value.length) {
+		for (var s in carets) {
+			if ((carets[sessionid].begin < carets[s].begin) && (codeEdit.selectionStart < carets[s].begin)) {
+				carets[s].begin = carets[s].begin + codeEdit.value.length - str;
+				if (carets[s].begin < carets[sessionid].begin)
+					carets[s].begin = carets[sessionid].begin
+				if (carets[s].begin > codeEdit.value.length)
+					carets[s].begin = codeEdit.value.length
+			}
+		}
+		for (var s in carets) {
+			if ((carets[sessionid].end < carets[s].end) && (codeEdit.selectionStart < carets[s].end)) {
+				carets[s].end = carets[s].end + codeEdit.value.length - str;
+				if (carets[s].end < carets[sessionid].end)
+					carets[s].end = carets[sessionid].end
+				if (carets[s].end > codeEdit.value.length)
+					carets[s].end = codeEdit.value.length
+			}
+		}
+		str = codeEdit.value.length
+		renderStr()
+		renderCarets()
+		socketCode.emit("message", {str: codeEdit.value, caret: carets[sessionid]})
+	}
+
+	if ((carets[sessionid].begin != codeEdit.selectionStart)||(carets[sessionid].end != codeEdit.selectionEnd)) {
+		carets[sessionid].begin = codeEdit.selectionStart
+		carets[sessionid].end = codeEdit.selectionEnd
+		renderCaret(sessionid)
+		socketCode.emit("position", {caret: carets[sessionid]} )
+	}
+
+}
+
+var socketCode = io.connect("http://code.temoffey.ru:8040"+window.location.pathname)
+
+socketCode.on("connect", function() {
+
+	sessionid = socketCode.socket.sessionid
+	Caret.id = "Caret-"+sessionid
+	editCaret.id = "editCaret-"+sessionid
+	carets[sessionid] = {begin: 0, end: 0}
+	setInterval(handler, 40)
+
+	socketCode.on("message", function (data) {
+		for (var s in carets) {
+			if ((carets[sessionid].begin < carets[s].begin) && (codeEdit.selectionStart < carets[s].begin)) {
+				carets[s].begin = carets[s].begin + codeEdit.value.length - str;
+				if (carets[s].begin < carets[sessionid].begin)
+					carets[s].begin = carets[sessionid].begin
+				if (carets[s].begin > codeEdit.value.length)
+					carets[s].begin = codeEdit.value.length
+			}
+		}
+		for (var s in carets) {
+			if ((carets[sessionid].end < carets[s].end) && (codeEdit.selectionStart < carets[s].end)) {
+				carets[s].end = carets[s].end + codeEdit.value.length - str;
+				if (carets[s].end < carets[sessionid].end)
+					carets[s].end = carets[sessionid].end
+				if (carets[s].end > codeEdit.value.length)
+					carets[s].end = codeEdit.value.length
+			}
+		}
+		codeEdit.value = data.str
+		str = codeEdit.value.length
+		codeEdit.selectionEnd = carets[sessionid].begin
+		codeEdit.selectionStart = carets[sessionid].end
+		renderStr()
+		renderCarets()
+	})
+
+	socketCode.on("position", function (data) {
+		carets[data.id] = data.caret
+		renderCaret(data.id)
+	})
+
+	socketCode.on("join", function (data) {
+		carets[data.id] = data.caret
+		editCaret.parentNode.insertAdjacentHTML("afterEnd", "<pre class=\"codeCaret\"><span class=\"editCaret\" id=\"editCaret-"+data.id+"\"></span><span class=\"Caret Caret-"+Math.floor(Math.random()*5+1)+"\" id=\"Caret-"+data.id+"\"></span></pre>")
+	})
+
+	socketCode.on("leave", function (data) {
+		delete carets[data.id]
+		document.getElementById("editCaret-"+data.id).parentNode.parentNode.removeChild(document.getElementById("editCaret-"+data.id).parentNode)
+	})
+
+})
+
+renderStr()
--- server.js
+++ server.js
@@ -8,65 +8,92 @@ var rooms = []
 
 server.listen(8040)
 
+var createRoom = function(id) {
+
+	rooms[id] = {str: "", carets: []}
+
+	rooms[id].io = io.of("/"+id).on("connection", function (socket) {
+
+		rooms[id].carets[socket.id] = {id: socket.id, begin: 0, end: 0}
+		socket.broadcast.emit("join", {id: socket.id, caret: rooms[id].carets[socket.id]})
+
+		socket.on("message", function (data) {
+			rooms[id].str = data.str
+			rooms[id].carets[socket.id] = data.caret
+			data.id = socket.id
+			socket.broadcast.emit("message", data)
+		})
+
+		socket.on("position", function (data) {
+			rooms[id].carets[socket.id] = data.caret
+			data.id = socket.id
+			socket.broadcast.emit("position", data)
+		})
+
+		socket.on("disconnect", function () {
+			delete rooms[id].carets[socket.id]
+			if (!Object.keys(rooms[id].carets).length) {
+				delete rooms[id].io
+			} else {
+				socket.broadcast.emit("leave", {id: socket.id})
+			}
+		})
+
+	})
+
+}
+
 app.use(express.static(__dirname + "/"))
 
 app.set("views", __dirname + "/")
 
 app.engine("ejs", require("ejs").renderFile)
 
-app.get("/:id?", function (req, res, next) {
+app.get("/:id?/:method?", function (req, res, next) {
 
-	if (req.params.id && rooms[req.params.id]) {
+	if (req.params.id) {
+		if (rooms[req.params.id]) {
+			if (req.params.method) {
+				if (req.params.method == "fork") {
 
-		res.render("index.ejs", {code: rooms[req.params.id].str, carets: rooms[req.params.id].carets})
+					id = Math.random().toString(36).substring(2, 9)
+					while (rooms[id]) {
+						id = Math.random().toString(36).substring(2, 9)
+					}
+					res.redirect('/'+id)
+					createRoom(id)
+					rooms[id].str = rooms[req.params.id].str
 
-	} else {
+				}
+				if (req.params.method == "close") {
 
-		if (!req.params.id) {
+					res.redirect('/about')
+					setTimeout(function() { delete rooms[req.params.id] }, 2000)		// Fix this, create method "leave"
 
-			req.params.id = Math.random().toString(36).substring(2, 9)
+				}
+			} else {
 
-			while (rooms[req.params.id]) {
-				req.params.id = Math.random().toString(36).substring(2, 9)
+				res.render("index.ejs", {id: req.params.id, code: rooms[req.params.id].str, carets: rooms[req.params.id].carets})
+
 			}
+		} else {
+
+			createRoom(req.params.id)
+			res.render("index.ejs", {id: req.params.id, code: rooms[req.params.id].str, carets: rooms[req.params.id].carets})
 
 		}
 
-		res.redirect('/'+req.params.id)
+	} else {
 
-		rooms[req.params.id] = {str: "", carets: [], socket: {}}
-
-		rooms[req.params.id].io = io.of("/"+req.params.id).on("connection", function (socket) {
-
-			rooms[req.params.id].carets[socket.id] = {id: socket.id, pos: 0}
-			socket.broadcast.emit("join", {id: socket.id})
-
-			socket.on("message", function (data) {
-				rooms[req.params.id].str = data.str
-				data.id = socket.id
-				rooms[req.params.id].carets[socket.id].pos = data.pos
-				socket.broadcast.emit("message", data)
-			})
-
-			socket.on("position", function (data) {
-				rooms[req.params.id].carets[socket.id].pos = data.pos
-				data.id = socket.id
-				socket.broadcast.emit("position", data)
-			})
-
-			socket.on("disconnect", function () {
-				delete rooms[req.params.id].carets[socket.id]
-				if (rooms[req.params.id].carets.length == 0) {
-					delete rooms[req.params.id].io
-				} else {
-					socket.broadcast.emit("leave", {id: socket.id})
-				}
-			})
-
-		})
+		id = Math.random().toString(36).substring(2, 9)
+		while (rooms[id]) {
+			id = Math.random().toString(36).substring(2, 9)
+		}
+		res.redirect('/'+id)
+		createRoom(id)
 
 	}
 
-});
+})
 
 io.set("log level", 1)
